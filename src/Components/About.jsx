edcb0ec65507624e2749792bc3239376e24cfd1a
apoorvpandey0 <apoorvpandey0@gmail.com>
2023-04-16 15:04:18 +0530

    fixed text size

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -7,12 +7,12 @@ const About = () => {
         <div className='pb-8'>
             <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
         </div>
-        <p className='text-xl mt-5 text-slate-200'>
+        <p className='text-l mt-5 text-slate-200'>
         Apoorv Pandey is a skilled and motivated software developer with experience in data science, mobile app development, and web development. With a Bachelor's degree in Information Technology from the University Institute of Technology RGPV, Bhopal, Apoorv has demonstrated his proficiency in programming languages such as Python and Dart. 
         He has also gained expertise in technologies such as Flutter, Django, and Azure, and has earned certifications in Azure Fundamentals, Azure AI Fundamentals, and Blockchain Specialization by Coursera. 
         </p>
         <br />
-        <p className='text-xl text-slate-200'>
+        <p className='text-l text-slate-200'>
         Apoorv has proven his ability to work efficiently on diverse projects, from developing and maintaining mobile applications with over 100,000 downloads to handling backend deployments on AWS. His dedication and commitment to his work have been recognized in his achievements, such as helping save 100+ hours of work time with automation during his internship at Bajaj Finserv Health. Apoorv is an enthusiastic learner and an excellent team player who enjoys sharing his knowledge through workshops and courses.
         </p>
       </div>
