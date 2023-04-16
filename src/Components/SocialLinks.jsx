65f57494dd013e44cfedd3e2b32418960801894b
apoorvpandey0 <apoorvpandey0@gmail.com>
2023-04-16 14:49:01 +0530

    fixed button rounding issue and hide sidebar in md

--- src/Components/SocialLinks.jsx
+++ src/Components/SocialLinks.jsx
@@ -2,60 +2,61 @@ import React from 'react';
 import { FaGithub , FaLinkedin } from 'react-icons/fa';
 import {HiOutlineMailOpen} from  'react-icons/hi'
 import {BsFillPersonLinesFill,BsYoutube} from 'react-icons/bs';
+
+const links = [
+    {
+        id:1,
+        child:(
+            <>
+            LinkedIn<FaLinkedin size={30}/>
+            </>
+        ),
+        href:'https://www.linkedin.com/in/apoorv-pandey/',
+        style:'rounded-tr-md'
+    },
+    {
+        id:2,
+        child:(
+            <>
+            Github<FaGithub size={30}/>
+            </>
+        ),
+        href:'https://github.com/apoorvpandey0'
+    },
+    {
+        id:3,
+        child:(
+            <>
+            Mail<HiOutlineMailOpen size={30}/>
+            </>
+        ),
+        href:'mailto: apoorvpandey0@gmail.com'
+    },
+    {
+        id:4,
+        child:(
+            <>
+           Resume <BsFillPersonLinesFill size={30}/>
+            </>
+        ),
+        href:'RESUME 14 April 2023.pdf',
+        download:true,
+    },
+    {
+        id:5,
+        child:(
+            <>
+           Youtube <BsYoutube size={30}/>
+            </>
+        ),
+        href:'https://www.youtube.com/channel/UCqt-XHfPFjSZPV8WqWaTV8Q',
+        style:'rounded-br-md',
+    }
+];
+
 const SocialLinks = () => {
-    const links = [
-        {
-            id:1,
-            child:(
-                <>
-                LinkedIn<FaLinkedin size={30}/>
-                </>
-            ),
-            href:'https://www.linkedin.com/in/apoorv-pandey/',
-            style:'rounded-tr-md'
-        },
-        {
-            id:2,
-            child:(
-                <>
-                Github<FaGithub size={30}/>
-                </>
-            ),
-            href:'https://github.com/apoorvpandey0'
-        },
-        {
-            id:3,
-            child:(
-                <>
-                Mail<HiOutlineMailOpen size={30}/>
-                </>
-            ),
-            href:'mailto: apoorvpandey0@gmail.com'
-        },
-        {
-            id:4,
-            child:(
-                <>
-               Resume <BsFillPersonLinesFill size={30}/>
-                </>
-            ),
-            href:'RESUME 14 April 2023.pdf',
-            style:'rounded-br-md',
-            download:true,
-        },
-        {
-            id:5,
-            child:(
-                <>
-               Youtube <BsYoutube size={30}/>
-                </>
-            ),
-            href:'https://www.youtube.com/channel/UCqt-XHfPFjSZPV8WqWaTV8Q',
-            style:'rounded-br-md',
-        }
-    ];
   return (
-    <div name='SocialLink' className='flex flex-col top-[35%] left-0 fixed'>
+    <div name='SocialLink' className='md:visible invisible flex flex-col top-[35%] left-0 fixed'>
       <ul>
         {links.map(({id, child, href, style, download}) =>(
             <li key={id} className= {" flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" +" "+ style}>
